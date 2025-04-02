1af91bb1de8a3bf6b6f67d6768ebefb2bab0d1cb
VERNAGUT-Titouan-2326112-A <titouan.vernagut@etu.univ-amu.fr>
2025-04-02 11:57:47 +0200

    modification de book a panier

--- src/main/java/fr/univamu/iut/CC2/PanierApplication.java
+++ src/main/java/fr/univamu/iut/CC2/PanierApplication.java
@@ -29,14 +29,8 @@ public class PanierApplication extends Application {
         }
         return db;
     }
-    /**
-     * Méthode appelée par l'API CDI pour injecter l'API Book au moment de la création de la ressource
-     * @return une instance de l'API avec l'url à utiliser
-     */
-    @Produces
-    private PanierRepositoryInterface connectBookApi(){
-        return new PanierRepositoryAPI("http://localhost:8080/Panier-1.0-SNAPSHOT/api/");
-    }
+
+
     /**
      * Méthode permettant de fermer la connexion à la base de données lorsque l'application est arrêtée
      * @param panierRepo la connexion à la base de données instanciée dans la méthode @openDbConnection
